613a83d8e506788038bfac755f4a66033976f58a
haairolla <vkurce@outlook.com>
2024-07-16 17:11:41 +0500

    KS-1-2 console output

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_1
+
+fun main() {
+    val numOfOrders = 75
+    val gratitudeTxt = "thanks for the purchase!"
+
+    println(numOfOrders)
+    println(gratitudeTxt)
+
+//    val numOfEmployees = 2000
+    val numOfEmployees = 1999
+    println(numOfEmployees)
+
+}
