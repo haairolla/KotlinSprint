ee5734e5c090614ceac284883e6abf093c8f17d6
haairolla <vkurce@outlook.com>
2024-07-16 19:20:09 +0500

    KS-1-5 time

---
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_1
+
+fun main() {
+
+    val seconds = 6480
+    val hour = "01"
+    val minute = "48"
+    val lastSeconds = "08"
+
+    print(hour + ":" + minute + ":" + lastSeconds)
+}
