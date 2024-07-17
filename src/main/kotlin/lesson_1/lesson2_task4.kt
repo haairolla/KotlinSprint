3028d42f84f1c1eabb593e9c3895e88733f17967
haairolla <vkurce@outlook.com>
2024-07-17 19:17:52 +0500

    KS-2-4 game

---
+++ src/main/kotlin/lesson_1/lesson2_task4.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_1
+
+fun main() {
+
+    val crystall = 7
+    val iron = 11
+
+    val crystalWithBaff = (crystall * 20) / 100
+    val ironWithBaff = (iron * 20) / 100
+
+    println("Бонус благодаря баффу: " + crystalWithBaff + " кристалл")
+    println("Бонус благодаря баффу: " + ironWithBaff + " железа")
+
+}
