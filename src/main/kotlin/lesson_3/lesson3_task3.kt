3a0a575d9c34e6af02866fbffd4b897af2f7e7b2
haairolla <vkurce@outlook.com>
2024-07-18 22:37:02 +0500

    KS-3-3 multiplication

---
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -0,0 +1,33 @@
+package org.example.lesson_3
+
+fun main() {
+
+    val sum = 5
+
+    val one = sum * 1
+    val two = sum * 2
+    val three = sum * 3
+    val four = sum * 4
+    val five = sum * 5
+    val six = sum * 6
+    val seven = sum * 7
+    val eight = sum * 8
+    val nine = sum * 9
+    val ten = sum * 10
+
+    println(
+        """
+            $sum x 1 = $one
+            $sum x 2 = $two
+            $sum x 3 = $three
+            $sum x 4 = $four
+            $sum x 5 = $five
+            $sum x 6 = $six
+            $sum x 7 = $seven
+            $sum x 8 = $eight
+            $sum x 9 = $nine
+            $sum x 10 = $ten
+        """.trimIndent()
+    )
+
+}
