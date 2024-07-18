c7f70ef21adc1972308f1827cbd220794e0c5b58
haairolla <vkurce@outlook.com>
2024-07-18 23:03:01 +0500

    KS-3-5 chess two

---
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main() {
+
+    val strokeData = "D2-D4;0"
+
+    var moveFrom = "D2"
+    var moveTo = "D4"
+    var strokeNum = 0
+
+    println(moveFrom)
+    println(moveTo)
+    println(strokeNum)
+
+}
