80e7acaf7c404cb0a255295e2392fea0f870dd10
haairolla <vkurce@outlook.com>
2024-07-18 22:18:53 +0500

    KS-3-1 greeting

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_3
+
+fun main() {
+
+    var helloUser = "Добрый день"
+    var user = "Николай!"
+
+    println("$helloUser, $user")
+
+    helloUser = "Добрый вечер"
+    println("$helloUser, $user")
+
+}
