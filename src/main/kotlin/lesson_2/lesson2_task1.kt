5a3347e9a4c23c01682c8a9ed85cb3de9f59f2c8
haairolla <vkurce@outlook.com>
2024-07-18 20:45:43 +0500

    KS-2-1 correction

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -2,7 +2,11 @@ package org.example.lesson_2
 
 fun main() {
 
-    val averageScore: Double = (3.0 + 4.0 + 3.0 + 5.0) / 4.0
+    val weaklyBall = 3.0
+    val wellBall = 4.0
+    val greatBall = 5.0
+
+    val averageScore: Double = (weaklyBall + wellBall + weaklyBall + greatBall) / 4.0
 
     print(averageScore)
 
