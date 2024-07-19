5362f5408a1f2c11bdc20656dcce15199baddca0
haairolla <vkurce@outlook.com>
2024-07-19 20:02:48 +0500

    KS-2-3 correction

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -4,18 +4,16 @@ fun main() {
 
     val trainExitHour = 9
     val trainExitMinute = 39
+    val totalMinuteSum = (trainExitHour * HOUR_TO_MINUTE) + trainExitMinute
+
 
     val travelTime = 457
-    val timeToHour = travelTime / 60
-    val timeToMinute = travelTime / timeToHour
+    val timeToMinute = travelTime % HOUR_TO_MINUTE
+    val arrivalTime = (totalMinuteSum + travelTime) % HOUR_TO_MINUTE
 
-    var totalTimeHour = trainExitHour + timeToHour
-    var totalTimeMinute = trainExitMinute + timeToMinute
-    if (totalTimeMinute >= 60) {
-        totalTimeMinute = totalTimeMinute - 60
-        totalTimeHour++
-    }
 
-    print(String.format(("%02d:%02d"), totalTimeHour, totalTimeMinute))
+    print(String.format(("%02d:%02d"), arrivalTime, timeToMinute))
 
 }
+
+const val HOUR_TO_MINUTE = 60
