3f11d01b6bb5af377bd0d13a3255f965e510bbc9
haairolla <vkurce@outlook.com>
2024-07-20 17:54:14 +0500

    KS-5-3 lottery

---
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -0,0 +1,22 @@
+package org.example.lesson_5
+
+fun main() {
+
+    var winNumOne = 22
+    var winNumTwo = 5
+
+    println("Нужно угадать два числа от 0 до 42")
+    print("Введите первое число: ")
+    val userNumOne = readLine()!!.toInt()
+    print("Введите второе число: ")
+    val userNumTwo = readLine()!!.toInt()
+
+    if (userNumOne == winNumOne && userNumTwo == winNumTwo) {
+        println("Поздравляем! Вы выиграли главный приз!")
+    } else if(userNumOne == winNumOne || userNumTwo == winNumTwo) {
+        println("Вы выиграли утешительный приз!")
+    } else if(userNumOne != winNumOne && userNumTwo != winNumTwo) {
+        println("Неудача!")
+    }
+
+}
