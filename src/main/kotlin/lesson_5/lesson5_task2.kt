5ed3f43c241f16f569931664edd5d8878bbe9292
haairolla <vkurce@outlook.com>
2024-07-20 17:27:58 +0500

    KS-5-2 age

---
+++ src/main/kotlin/lesson_5/lesson5_task2.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_5
+
+fun main() {
+
+    print("Введите ваш год рождения: ")
+    val userYear = readLine()!!.toInt()
+    val userAge = THIS_YEAR - userYear
+
+    if(userAge >= USER_AGE_MIN) {
+        println("Показать экран со скрытым контентом")
+    } else {
+        println("Доступ запрещен!")
+    }
+
+
+}
+
+const val THIS_YEAR = 2024
+const val USER_AGE_MIN = 18
