921788d789c7239b6356dce9985ee6430cd8ec2c
haairolla <vkurce@outlook.com>
2024-07-22 14:38:57 +0500

    KS-4-3 correction

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -7,11 +7,11 @@ fun main() {
     val todayWetness: Int = 20
     val todaySeason: String = "зима"
 
-    println("Благоприятные ли условия сейчас для роста бобовых? ${isSunnyToday == isSunny && isTentOpenToday == isTentOpen && todayWetness == needWetness && todaySeason == needSeason}")
+    println("Благоприятные ли условия сейчас для роста бобовых? ${isSunnyToday == IS_SUNNY && isTentOpenToday == IS_TENT_OPEN && todayWetness == NEED_WETNESS && todaySeason == NEED_SEASON}")
 
 }
 
-const val isSunny: Boolean = true
-const val isTentOpen: Boolean = true
-const val needWetness = 20
-const val needSeason = "Не зима"
+const val IS_SUNNY: Boolean = true
+const val IS_TENT_OPEN: Boolean = true
+const val NEED_WETNESS = 20
+const val NEED_SEASON = "Не зима"
