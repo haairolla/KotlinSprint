58528fccd08a168ca4fb468c2a6509c04abf239a
haairolla <vkurce@outlook.com>
2024-07-25 23:57:41 +0500

    KS-4-4 correction

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -2,15 +2,15 @@ package org.example.lesson_4
 
 fun main() {
 
-    val day = 1
-    val isEven = day % 2 == 1
-    val isNotEven = day % 2 == 0
+    val day = 5
+    val isEven = day % 2 == 0
+
 
 
     println("""
         Упражнения для рук:    $isEven
-        Упражнения для ног:    $isNotEven
-        Упражнения для спины:  $isNotEven
+        Упражнения для ног:    ${!isEven}
+        Упражнения для спины:  ${!isEven}
         Упражнения для пресса: $isEven
     """.trimIndent())
 
